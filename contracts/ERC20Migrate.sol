2deb405176f478daf033be7244f939caf067e17d
Terry Wilkinson <terry@polytreelabs.com>
2020-04-19 14:43:53 +0800

    Add LICENSE information, TODO - adujst file structure to remove redundancy, properly import openzeppelin dependencies

--- contracts/ERC20Migrate.sol
+++ contracts/ERC20Migrate.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.5.0;
 
-import "./token_migrate/TokenMigrate.sol";
-import "./token_migrate/StandardMigration.sol";
+import "./migrate/TokenMigrate.sol";
+import "./migrate/StandardMigration.sol";
 
 contract ERC20Migrate is TokenMigrate, StandardMigration {
     
--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,3 +1,4 @@
+// THIS FILE IS UNUSED IT I STRICTLY HERE TO PASS TRUFFLE TESTING
 pragma solidity ^0.5.16;
 
 contract Migrations {
